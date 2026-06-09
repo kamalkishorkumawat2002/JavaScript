ad6733bbeaf44a685b37aeeb74e1b9b84f56232d
kamalkishorkumawat2002 <kamalkishorkumawat2002@gmail.com>
2026-06-09 17:10:37 +0530

    02_datatyes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,3 @@
+// alert(4+4) //this will not allowed, because alert is not defined in Node.js environment. It is a browser-specific function. In Node.js, you can use console.log() instead to display output.
+
+console.log(4+4) // This will output 8 to the console
